e7e6736ea78775f62db79fc056fcaaa358c0a3ee
yusha-g <pratyusha.gh@gmail.com>
2024-04-13 13:51:18 +0530

    feat: implement autoactivate

--- cd_into_env.sh
+++ cd_into_env.sh
@@ -2,11 +2,23 @@ cd_into_env(){
     buildin cd "@"
     # check if .python-version exists in current dir
     # if exists, read venv name
-
-    # check if no virtual env is active
-        # activate venv read from .python-version
-
-    # if virtual env is active
-        # deactivate current env
-        # activate venv read from .python-version
+    if [ -e .python-version ] ; then
+        env_name="`cat .python-version`"
+        env_path=my_venvs/$env_name
+        # check if no virtual env is active
+        if [ -z "$VIRTUAL_ENV" ] ; then
+            # activate venv read from .python-version
+            if [ -d $env_path ] ; then
+                source $env_path/bin/activate
+            fi
+        else
+            # if virtul_env is active, check if folder is a subdir of original virtual_env
+            # if yes then do nothing
+            # else deactivate
+            parentdir="$(dirname "$VIRTUAL_ENV")"
+            if [[ "$PWD"/ != "$parentdir"/* ]] ; then
+                deactivate
+            fi
+        fi
+    fi
 }
