4cdd8e4123d5a0e277a1b17e7da77a0f638888be
yusha-g <pratyusha.gh@gmail.com>
2024-04-13 20:24:48 +0530

    feat: implement way to autodeactivate

--- cd_into_env.sh
+++ cd_into_env.sh
@@ -1,17 +1,20 @@
-cd_into_env(){
-    buildin cd "@"
+cd(){
+    # TODO: there's a lot of redundant code. come up with a way to fix it. 
+    builtin cd "$@"
     # check if .python-version exists in current dir
     # if exists, read venv name
     if [ -e .python-version ] ; then
         env_name="`cat .python-version`"
-        env_path=my_venvs/$env_name
+        env_path=~/venv-manager/my_venvs/$env_name
         # check if no virtual env is active
         if [ -z "$VIRTUAL_ENV" ] ; then
             # activate venv read from .python-version
             if [ -d $env_path ] ; then
                 source $env_path/bin/activate
             fi
-        else
+        fi
+    else
+        if [ ! -z "$VIRTUAL_ENV" ] ; then
             # if virtul_env is active, check if folder is a subdir of original virtual_env
             # if yes then do nothing
             # else deactivate
